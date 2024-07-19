1570546a5e92ffcef7ae9961508c56c49ae442e9
Sondre Aasemoen <sondre@eons.io>
2024-07-19 20:21:49 +0200

    Add rest of tags from MDN

--- tags.ts
+++ tags.ts
@@ -1,3 +1,13 @@
+import { label } from "./html_tags.ts";
+import { textarea } from "./html_tags.ts";
+import { video } from "./html_tags.ts";
+import { template } from "./html_tags.ts";
+import { img } from "./html_tags.ts";
+import { audio } from "./html_tags.ts";
+import { select } from "./html_tags.ts";
+import { i } from "./html_tags.ts";
+import { em } from "./html_tags.ts";
+
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element
 export const HTML_TAGS = {
   html: "html",
@@ -272,3 +282,196 @@ export type HeadingContent = keyof typeof HEADING_CONTENT;
 export const headingContentTags: Array<HeadingContent> = Object.keys(HEADING_CONTENT) as Array<HeadingContent>;
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Content_categories#phrasing_content
+export const PHRASING_CONTENT = {
+  abbr: "abbr",
+  audio: "audio",
+  b: "b",
+  bdi: "bdi",
+  bdo: "bdo",
+  br: "br",
+  button: "button",
+  canvas: "canvas",
+  cite: "cite",
+  code: "code",
+  data: "data",
+  datalist: "datalist",
+  dfn: "dfn",
+  em: "em",
+  embed: "embed",
+  i: "i",
+  iframe: "iframe",
+  img: "img",
+  input: "input",
+  kbd: "kbd",
+  label: "label",
+  mark: "mark",
+  math: "math",
+  meter: "meter",
+  noscript: "noscript",
+  object: "object",
+  output: "output",
+  picture: "picture",
+  progress: "progress",
+  q: "q",
+  ruby: "ruby",
+  s: "s",
+  samp: "samp",
+  script: "script",
+  select: "select",
+  slot: "slot",
+  small: "small",
+  span: "span",
+  strong: "strong",
+  sub: "sub",
+  sup: "sup",
+  svg: "svg",
+  template: "template",
+  textarea: "textarea",
+  time: "time",
+  u: "u",
+  var: "var",
+  video: "video",
+  wbr: "wbr",
+  a: "a",
+  del: "del",
+  ins: "ins",
+};
+
+export type PhrasingContent = keyof typeof PHRASING_CONTENT;
+export const phrasingContentTags: Array<PhrasingContent> = Object.keys(PHRASING_CONTENT) as Array<PhrasingContent>;
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Content_categories#embedded_content
+export const EMBEDDED_CONTENT = {
+  audio: "audio",
+  canvas: "canvas",
+  embed: "embed",
+  iframe: "iframe",
+  img: "img",
+  math: "math",
+  object: "object",
+  picture: "picture",
+  svg: "svg",
+  video: "video",
+};
+
+export type EmbeddedContent = keyof typeof EMBEDDED_CONTENT;
+export const embeddedContentTags: Array<EmbeddedContent> = Object.keys(EMBEDDED_CONTENT) as Array<EmbeddedContent>;
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Content_categories#interactive_content
+export const INTERACTIVE_CONTENT = {
+  a: "a",
+  button: "button",
+  details: "details",
+  embed: "embed",
+  iframe: "iframe",
+  label: "label",
+  select: "select",
+  audio: "audio",
+  video: "video",
+};
+
+export type InteractiveContent = keyof typeof INTERACTIVE_CONTENT;
+export const interactiveContentTags: Array<InteractiveContent> = Object.keys(INTERACTIVE_CONTENT) as Array<
+  InteractiveContent
+>;
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Content_categories#palpable_content
+export const PALPABLE_CONTENT = {
+  a: "a",
+  abbr: "abbr",
+  address: "address",
+  article: "article",
+  aside: "aside",
+  b: "b",
+  bdi: "bdi",
+  bdo: "bdo",
+  blockquote: "blockquote",
+  button: "button",
+  canvas: "canvas",
+  cite: "cite",
+  code: "code",
+  data: "data",
+  del: "del",
+  details: "details",
+  dfn: "dfn",
+  div: "div",
+  em: "em",
+  embed: "embed",
+  fieldset: "fieldset",
+  footer: "footer",
+  figure: "figure",
+  form: "form",
+  iframe: "iframe",
+  img: "img",
+  ins: "ins",
+  kbd: "kbd",
+  label: "label",
+  main: "main",
+  map: "map",
+  mark: "mark",
+  math: "math",
+  meter: "meter",
+  nav: "nav",
+  object: "object",
+  p: "p",
+  picture: "picture",
+  pre: "pre",
+  progress: "progress",
+  q: "q",
+  ruby: "ruby",
+  s: "s",
+  samp: "samp",
+  search: "search",
+  section: "section",
+  select: "select",
+  small: "small",
+  span: "span",
+  strong: "strong",
+  sub: "sub",
+  sup: "sup",
+  svg: "svg",
+  table: "table",
+  textarea: "textarea",
+  time: "time",
+  u: "u",
+  var: "var",
+  video: "video",
+  audio: "audio",
+  dl: "dl",
+  input: "input",
+  ol: "ol",
+  ul: "ul",
+};
+
+export type PalpableContent = keyof typeof PALPABLE_CONTENT;
+export const palpableContentTags: Array<PalpableContent> = Object.keys(PALPABLE_CONTENT) as Array<PalpableContent>;
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Content_categories#form-associated_content
+export const FORM_ASSOCIATED_CONTENT = {
+  button: "button",
+  fieldset: "fieldset",
+  input: "input",
+  label: "label",
+  meter: "meter",
+  object: "object",
+  output: "output",
+  progress: "progress",
+  select: "select",
+  textarea: "textarea",
+};
+
+export type FormAssociatedContent = keyof typeof FORM_ASSOCIATED_CONTENT;
+export const formAssociatedContentTags: Array<FormAssociatedContent> = Object.keys(FORM_ASSOCIATED_CONTENT) as Array<
+  FormAssociatedContent
+>;
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Content_categories#script-supporting_elements
+export const SCRIPT_SUPPORTING_ELEMENTS = {
+  script: "script",
+  template: "template",
+};
+
+export type ScriptSupportingElement = keyof typeof SCRIPT_SUPPORTING_ELEMENTS;
+export const scriptSupportingElementTags: Array<ScriptSupportingElement> = Object.keys(
+  SCRIPT_SUPPORTING_ELEMENTS,
+) as Array<ScriptSupportingElement>;
