9df5fa994194a200a47be4f2e3bf99cd7242db3d
Sondre Aasemoen <sondre@eons.io>
2024-07-22 14:53:15 +0200

    Add NPM build script

---
+++ scripts/build_npm.ts
@@ -0,0 +1,43 @@
+import { build, emptyDir } from "jsr:@deno/dnt";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  importMap: "./deno.jsonc",
+  esModule: true,
+  outDir: "./npm",
+  shims: {
+    deno: {
+      test: true
+    }
+  },
+    compilerOptions: {
+    target: "ES2021",
+    sourceMap: true,
+  },
+  package: {
+    name: "hdsl",
+    version: Deno.args[0]?.replace(/^v/, ""),
+    author: "Sondre Aasemoen <sondre@eons.io> (https://www.eons.io)",
+    description: "A simple, type-safe, and functional way to create HTML documents in TypeScript.",
+    license: "MIT",
+    homepage: "https://github.com/sondr3/hdsl#readme",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/sondr3/hdsl.git",
+    },
+    bugs: {
+      url: "https://github.com/sondr3/hdsl/issues",
+    },
+    keywords: ["html", "dsl", "typescript", "type-safe", "template"],
+    engines: {
+      node: ">=18.0.0",
+    },
+  },
+  postBuild: async () => {
+    await Deno.copyFile("./README.md", "./npm/README.md");
+    await Deno.copyFile("./LICENSE", "./npm/LICENSE");
+    await Deno.copyFile("./CHANGELOG.md", "./npm/CHANGELOG.md");
+  }
+})
