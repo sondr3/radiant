d01640cd23110ca8f590aff64d6220f93ee395d9
Sondre Aasemoen <sondre@eons.io>
2024-07-31 19:32:33 +0200

    Export CHILDREN_MAP in index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,4 +30,5 @@ export * from "./attributes.js";
 export * from "./html_element.js";
 export * from "./render_html.js";
 export * from "./tags.js";
+export type { CHILDREN_MAP } from "./content_categories.js";
 export type { ELEMENT_MAP } from "./elements.js";
